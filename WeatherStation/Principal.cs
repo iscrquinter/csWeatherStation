0d5ba7ce8a17c2433aff10f59676d0bbddf59fe1
Ricardo Quintero <rquintero@itculiacan.edu.mx>
2018-03-09 10:54:42 -0700

    Inicializando la App ...

---
+++ WeatherStation/Principal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherStation
+{
+    class Principal
+    {
+        public static void Main()
+        {
+            Console.WriteLine("The Weather Station App...");
+            Console.ReadLine();
+        }
+    }
+}
